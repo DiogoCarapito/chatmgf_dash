55b9ae7a6a325d52e6a8f8922f7ce8c56975953c
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-03-09 21:44:45 +0000

    building up the basics

--- src/app.py
+++ src/app.py
@@ -1,18 +1,82 @@
 import dash
 from dash import Input, Output, State, html
-#from dash import dcc
-#import dash_bootstrap_components as dbc
+from dash import dcc
+import dash_bootstrap_components as dbc
 
 #import requests
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SIMPLEX,dbc.icons.BOOTSTRAP])
 server = app.server
+
+navbar_pages = dbc.Row([
+    dbc.Col([
+        dbc.Nav([
+            dbc.NavItem(dbc.NavLink(page['name'], href=page['path']))
+            for page in dash.page_registry.values()
+        ]+[
+            dbc.NavItem(dbc.NavLink(html.I(
+                # children='source code',
+                className='bi bi-twitter'),
+                href='https://twitter.com/diogocarapito',
+                external_link=True)),
+            dbc.NavItem(dbc.NavLink(html.I(
+                #children='source code',
+                className='bi bi-github'),
+                href='https://github.com/DiogoCarapito/',
+                external_link=True)),
+        ], navbar=True,)
+    ]),
+])
+
+navbar = dbc.Navbar([
+    dbc.Container([
+        html.A(
+            dbc.Row([
+                dbc.Col(html.Img(src=app.get_asset_url('mgfchat-logo.png'), height='30px')),
+                dbc.Col(dbc.NavbarBrand('chatmgf', className='ms-2')),
+                ], align='center', className='g-0'),
+            href='/',
+            style={'textDecoration': 'none'},
+        ),
+        dbc.NavbarToggler(id='navbar-toggler', n_clicks=0),
+        dbc.Collapse(
+            navbar_pages,
+            id='navbar-collapse',
+            is_open=False,
+            navbar=True,
+        ),
+    ], fluid=True,),
+],
+    color='light',
+    dark=False,
+    fixed='top',
+)
 
 
 app.layout = html.Div([
-    html.H1('Hello, World!!'),
-    html.H2('Hello, World!!'),
+    navbar,
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    dbc.Container([
+        dash.page_container,
+    ]),
+
 ])
+
+@app.callback(
+    Output('navbar-collapse', 'is_open'),
+    Input('navbar-toggler', 'n_clicks'),
+    State('navbar-collapse', 'is_open'),
+)
+
+# Função para gerir o botão navbar colapse
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
 
 
 if __name__ == "__main__":
---
+++ src/pages/home.py
@@ -0,0 +1,23 @@
+import dash
+from dash import Dash, dcc, html, callback, Input, Output
+import dash_bootstrap_components as dbc
+
+
+dash.register_page(
+    __name__,
+    path='/',
+    title='home',
+    name='',
+    order=1,
+)
+
+container = dbc.Container([
+    html.H3('home'),
+], fluid=True)
+
+
+def layout():
+    return html.Div([
+        container,
+        html.Br(),
+    ])
