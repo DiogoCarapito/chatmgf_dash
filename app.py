a87c99efc11569a33a774e2cee6d7f3dafbcfcd9
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-03-09 19:10:49 +0000

    dash implementation

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import dash
+from dash import Input, Output, State, html
+from dash import dcc
+import dash_bootstrap_components as dbc
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+import requests
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+    html.H1('Hello, World!!'),
+])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
